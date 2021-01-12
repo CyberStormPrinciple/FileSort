d495be5ade25d17de11f8ec8bc3a407c763dc590
Nathan Gilbert <weneedmoreminerals@gmail.com>
2021-01-12 01:30:06 -0500

    Create folders and sub directories of file types

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # File sorting program
 
-Using cmd line arguments
+Using cmd line arguments 
 
 
 # To run:
 Remember to add the quote marks for each path to ensure success
-`python main.sort.py 'directory_path', 'destination_path', 'filter_file_path'`
+`python main.sort.py sorted_files 'directory_path' 'destination_path' 'filter_file_path'`
 Though destination_path and filter_file_path are optional
 If destination_path is not given the destination will be set to the directory_path given
-`python main.sort.py 'directory_path', 'destination_path'`
+`python main.sort.py sorted_files 'directory_path', 'destination_path'`
 If filter_file_path is not given you will be asked to enter the file types you want to filter
-`python main.sort.py 'directory_path', 'filter_file_path'`
+`python main.sort.py sorted_files 'directory_path', 'filter_file_path'`
--- main.sort.py
+++ main.sort.py
@@ -3,15 +3,6 @@
 import shutil, os, sys
 
 
-def get_file_type(file_name):
-  print(file_name)
-  type = file_name.split('.')
-  if len(type) < 2:
-    pass
-  else:
-    return file_name.split('.')[1]
-
-
 def check_directory(directory):
   return os.path.isdir(directory)
 
@@ -19,15 +10,68 @@ def check_directory(directory):
 def check_file(file):
   return os.path.isfile(file)
 
-# TODO
-def create_folders(filter, destination):
-  folder_path = ''
-  # Move folder to designated directory
-  dest = shutil.move(folder_path, destination)
 
-# TODO
+def create_folders(main_folder, destination, filter_set):
+  image_filetypes = ['tif', 'tiff', 'gif', 'jpeg', 'jpg', 'jif', 'jfif', 'jp2', 'jpx', 'j2k', 'j2c', 'fpx', 'pcd', 'png', 'ai', 'psd']
+  document_filetypes = ['xls', 'doc', 'docx', 'pdf', 'txt', 'xlsx', 'xlsm', 'ppt', 'pps', 'pptx']
+  audio_filetypes = ['mp3', 'aac', 'ac3', 'wav', 'wma', 'ogg', 'midi', 'mid', 'cda', 'aif']
+  video_filetypes = ['mp4', 'h264', 'avi', 'mkv', 'mpeg', 'mpg', 'mov', 'm4v', 'flv', '3gp', 'wmv', 'vob']
+  filetype_directories = ['Image files', 'Document files', 'Audio files', 'Video files', 'Misc files']
+  main_directory = f'{destination}\{main_folder}'
+  sub_directories = {}
+  try:
+    # Creates main parent folder at target directory
+    os.makedirs(main_directory)
+  except FileExistsError:
+      print(f'{main_directory} already exists')
+  finally:
+    for name in filetype_directories:
+      try:
+        # Creates sub directories in main folder
+        os.makedirs(f'{main_directory}\{name}')
+      except FileExistsError:
+        print(f'{main_directory}\{name} already exists')
+  for file_type in filter_set:
+    # Seperates the file types into their respective folder
+    if file_type in image_filetypes:
+      sub_directories[file_type] = create_subdirectory(
+        main_directory, 'Image files', file_type
+      )
+    elif file_type in document_filetypes:
+      sub_directories[file_type] = create_subdirectory(
+          main_directory, 'Document files', file_type
+      )
+    elif file_type in audio_filetypes:
+      sub_directories[file_type] = create_subdirectory(
+        main_directory, 'Audio files', file_type
+      )
+    elif file_type in video_filetypes:
+      sub_directories[file_type] = create_subdirectory(
+        main_directory, 'Video files', file_type
+      )
+    else:
+      sub_directories[file_type] = create_subdirectory(
+        main_directory, 'Misc files', file_type
+      )
+  return sub_directories
+
+
+def create_subdirectory(main_directory, sub_directory, file_type):
+  try:
+    file_type = file_type.upper()
+    file_type_path = f'{main_directory}\{sub_directory}\{file_type}'
+    # Creates folder at target directory for file type
+    os.makedirs(file_type_path)
+  except FileExistsError:
+    print(f'{main_directory}\{sub_directory} files already exists')
+  return file_type_path
+
+
 def read_filter(filter):
-  types = {}
+  types = set()
+  file = open(filter, 'r')
+  for file_type in file:
+    types.add(file_type.replace('\n', ''))
   return types
 
 
@@ -41,7 +85,13 @@ def make_filter():
   return set(list_types)
 
 
-def vaild_inputs(directory, destination, filter_path):
+def vaild_inputs(main_folder, directory, destination, filter_path):
+  for character in main_folder:
+    if character == '_':
+      pass
+    elif not character.isalnum():
+      print('Name of main folder is invalid')
+      sys.exit()
   if destination is None:
     destination = directory
   # Checks that all inputs are valid
@@ -50,7 +100,7 @@ def vaild_inputs(directory, destination, filter_path):
       if check_file(filter_path):
         pass
       else:
-        print('File_filter path is invalid')
+        print('File filter\'s path is invalid')
         sys.exit()
     if check_directory(destination):
       pass
@@ -60,15 +110,17 @@ def vaild_inputs(directory, destination, filter_path):
   else:
     print('Directory path is invalid')
     sys.exit()
+  return destination
 
 
-def main(directory, destination=None, filter_path=None):
-  vaild_inputs(directory, destination, filter_path)
+def main(main_folder, directory, destination=None, filter_path=None):
+  destination = vaild_inputs(main_folder, directory, destination, filter_path)
   if filter_path is not None:
     file_filter = read_filter(filter_path)
   else:
     file_filter = make_filter()
-  print(file_filter)
+  sub_directories = create_folders(main_folder, destination, file_filter)
+  print(sub_directories)
   print('success')
 
   # for path, dirs, files in os.walk(source):
@@ -77,25 +129,18 @@ def main(directory, destination=None, filter_path=None):
   #     print(os.path.join(path, file))
   #     set.add(get_file_type(file))
 
-# print(set)
-
-# pdf png jpeg 
-
-# source = 'D:\MP3\'s'
-
-# destination = 'D:\test'
 
 
 if __name__ == '__main__':
-  if len(sys.argv) == 2:
-    main(*sys.argv)
-  elif len(sys.argv) == 3:
+  if len(sys.argv) == 3:
+    main(*sys.argv[1:])
+  elif len(sys.argv) == 4:
     # Check if destiation or filter_path is provided
-    if check_directory(sys.argv[2]):
+    if check_directory(sys.argv[3]):
       main(*sys.argv[1:])
     else:
-      main(sys.argv[1], None, sys.argv[2])
-  elif len(sys.argv) == 4:
+      main(sys.argv[1], sys.argv[2], None, sys.argv[3])
+  elif len(sys.argv) == 5:
     main(*sys.argv[1:])
   else:
     print("Err")
